f908439a1dbdaa2ffc5d66334d0793e715ef75e7
jissp <jissp@naver.com>
2025-12-09 09:00:54 +0900

    feature: KoreaInvestment Websocket 추가
    - 일부 코드 리팩토링

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const logger = new Logger('Bootstrap');
+    const appPort = process.env.PORT ?? 3100;
+
+    const app = await NestFactory.create(AppModule);
+    await app.listen(appPort);
+
+    logger.log(`Server is running on port ${appPort}`);
 }
 bootstrap();
