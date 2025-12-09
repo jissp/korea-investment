4f99e0a7f5cea8398630e6cd89505a818507fd2c
jissp <jissp@naver.com>
2025-12-09 09:05:22 +0900

    refact: 일부 코드 정리

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
-import { AppModule } from './app/app.module';
+import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
     const logger = new Logger('Bootstrap');
