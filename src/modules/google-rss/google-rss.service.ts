f774a4c5b09bb91314eeea0f1c312011af32ffda
jissp <jissp@naver.com>
2026-02-11 13:23:24 +0900

    feature: 구글 뉴스 RSS 수집
    모듈 추가
    - Rss Reader
    - Xml Parser
    - Google Rss

---
+++ src/modules/google-rss/google-rss.service.ts
@@ -0,0 +1,25 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { RssReaderService } from '@modules/rss-reader';
+import {
+    GoogleRssConfig,
+    GoogleRssItem,
+    GoogleRssProvider,
+} from './google-rss.types';
+
+@Injectable()
+export class GoogleRssService {
+    constructor(
+        private readonly rssReaderService: RssReaderService,
+        @Inject(GoogleRssProvider.Config)
+        private readonly config: GoogleRssConfig,
+    ) {}
+
+    /**
+     * Google News에서 비즈니스 뉴스를 조회합니다.
+     */
+    public async getBusinessNews(): Promise<GoogleRssItem[]> {
+        return await this.rssReaderService.readFeed<GoogleRssItem>(
+            this.config.business,
+        );
+    }
+}
---
+++ src/modules/rss-reader/index.ts
@@ -0,0 +1,3 @@
+export { RssReaderProvider, RssChannel, RssFeed } from './rss-reader.types';
+export { RssReaderService } from './rss-reader.service';
+export { RssReaderModule } from './rss-reader.module';
